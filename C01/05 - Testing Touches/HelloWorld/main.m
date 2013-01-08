ba1e06d2ea66afd55fc7c00871976562860fafc6
Yehnan Chiang <yehnan174@gmail.com>
2013-01-08 16:14:00 +0800

    C01/05 comments translated to Chinese

--- C01/05 - Testing Touches/HelloWorld/main.m
+++ C01/05 - Testing Touches/HelloWorld/main.m
@@ -38,25 +38,25 @@
 	CGPoint pt;
 	float HALFSIDE = SIDELENGTH / 2.0f;
 	
-	// normalize with centered origin
+	// 將圓點當做中心，正規化
 	pt.x = (point.x - HALFSIDE) / HALFSIDE;
 	pt.y = (point.y - HALFSIDE) / HALFSIDE;
 	
-	// x^2 + y^2 = radius
+	// x^2 + y^2 = radius（半徑）
 	float xsquared = pt.x * pt.x;
 	float ysquared = pt.y * pt.y;
 	
-	// If the radius < 1, the point is within the clipped circle
+	// 若半徑小於或等於1，該點落於圓圈內
 	if ((xsquared + ysquared) < 1.0) return YES;
 	return NO;
 }
 
 - (void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    // Promote the touched view
+    // 將被觸摸到的視圖帶到最前面
     [self.superview bringSubviewToFront:self];
     
-    // Remember original location
+    // 記住原來的位置
     previousLocation = self.center;
 }
 
@@ -65,7 +65,7 @@
 	CGPoint translation = [uigr translationInView:self.superview];
 	CGPoint newcenter = CGPointMake(previousLocation.x + translation.x, previousLocation.y + translation.y);
 	
-	// Bound movement into parent bounds
+	// 將移動範圍限制在父視圖的bounds內
 	float halfx = CGRectGetMidX(self.bounds);
 	newcenter.x = MAX(halfx, newcenter.x);
 	newcenter.x = MIN(self.superview.bounds.size.width - halfx, newcenter.x);
@@ -74,7 +74,7 @@
 	newcenter.y = MAX(halfy, newcenter.y);
 	newcenter.y = MIN(self.superview.bounds.size.height - halfy, newcenter.y);
 	
-	// Set new location
+	// 設定新位置
 	self.center = newcenter;	
 }
 @end
@@ -101,13 +101,13 @@
 	UIGraphicsBeginImageContext(CGSizeMake(SIDELENGTH, SIDELENGTH));
 	CGContextRef context = UIGraphicsGetCurrentContext();
 	
-	// Create a filled ellipse
+	// 建立填滿顏色的橢圓
 	[color setFill];
 	CGRect rect = CGRectMake(0.0f, 0.0f, SIDELENGTH, SIDELENGTH);
 	CGContextAddEllipseInRect(context, rect);
 	CGContextFillPath(context);
 	
-	// Outline the circle a couple of times
+	// 繪製圓圈外圍，數次
 	CGContextSetStrokeColorWithColor(context, [[UIColor whiteColor] CGColor]);
 	CGContextAddEllipseInRect(context, CGRectInset(rect, INSET_AMT, INSET_AMT));
 	CGContextStrokePath(context);
@@ -123,7 +123,7 @@
 {
     self.view.backgroundColor = [UIColor blackColor];
 
-    // Add the circles to random points on the screen	
+    // 在螢幕上的亂數位置加入圓圈
     for (int i = 0; i < MAXCIRCLES; i++)
     {
         DragView *dragger = [[DragView alloc] initWithImage:[self createImage]];
